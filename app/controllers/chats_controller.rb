9c41054d61806a775b9be78b49c50a8b3319cb0a
AlaaElattar <alaamahmoud.1223@gmail.com>
2024-05-23 12:50:42 +0300

    fix creation method of chats

--- app/controllers/chats_controller.rb
+++ app/controllers/chats_controller.rb
@@ -1,15 +1,20 @@
 class ChatsController < ApplicationController
-    # handle pagination for large number of chats
+    protect_from_forgery with: :null_session, if: -> { request.format.json? }
     skip_before_action :verify_authenticity_token, only: [:create]
+
+    # TODO: creates right, but same chat id counter(not incrementing)
+    # POST /applications/:application_token/chats
     def create
-        @application = Application.find_by(token: params[:token])
-        if @application
-            chat = @application.chats.create(number: @application.chats.count + 1)
+        get_application_id
+        if @application_id
+            chat = Chat.create(application_id: @application_id, number: Chat.where(application_id: @application_id).count + 1)
+            puts "chat.number: #{chat.number}"
             render json: { number: chat.number }, status: :created
         else
             render json: { error: "Invalid application token" }, status: :unprocessable_entity
         end
     end
+
 
     def show
         application = Application.find_by(token: params[:token])
@@ -20,4 +25,10 @@ class ChatsController < ApplicationController
             render json: { error: "Invalid application token" }, status: :unprocessable_entity
         end
     end
+
+    private
+
+    def get_application_id
+        @application_id = Application.where(token: params[:application_id])
+    end    
 end
